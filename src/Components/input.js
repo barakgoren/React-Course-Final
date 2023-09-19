4f2f9dfc94bb869f3e56c859d581e69759c9657f
Barak Goren <barak.goren6@gmail.com>
2023-09-20 02:02:14 +0300

    Connect to API remove strict mode

--- src/Components/input.js
+++ src/Components/input.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CurrencyInput from './currencyInput'
 import { LiaExchangeAltSolid } from 'react-icons/lia'
 
 export default function Input() {
+    useEffect(() => {
+        doApi();
+    })
+    const doApi = async() => {
+        const url = 'https://monkeys.co.il/api2/currency.php';
+        const resp = await fetch(url);
+        const data = await resp.json();
+        console.log(data);
+    }
     return (
-        <div className='row'>
-            <h1 className='display-4 text-center'><span className='blinking-text'>Live</span> <br />Currency Exchange</h1>
+        <div className=''>
+            <h1 className='display-1 text-center'><span className='blinking-text'>Live</span> <br />Currency Exchange</h1>
             <div className='p-5 row justify-content-center'>
-                <CurrencyInput />
+                <CurrencyInput/>
                 <button className='btn col-1'><LiaExchangeAltSolid /></button>
                 <CurrencyInput />
             </div>
--- src/Components/score.js
+++ src/Components/score.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { FaArrowDown } from 'react-icons/fa'
 
 export default function Score() {
-  return (
-    <div>Score</div>
-  )
+    return (
+        <div>
+            <div className='p-5 row justify-content-center'>
+                <p className='display-6 text-center pb-5'>5 USD is</p>
+                <p className='display-6 text-center'><FaArrowDown/></p>
+                <p className='display-6 text-center p-5'>5 USD is</p>
+            </div>
+        </div>
+    )
 }
